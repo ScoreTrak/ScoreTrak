e1c5afd9b4faf39e5863a2fd8ffbdb9008e2d1d6
l1ghtman2k <aibek.zhil@gmail.com>
2020-04-28 07:37:48 -0400

    Use user's Built in Create method to avoid duplicate data.

--- web/scoretrak/grifts/db.go
+++ web/scoretrak/grifts/db.go
@@ -3,13 +3,17 @@ package grifts
 import (
 	. "github.com/markbates/grift/grift"
 	"scoretrak/models"
+	
 )
 
 var _ = Namespace("user", func() {
 	Desc("admin", "Generates admin account with Username: admin, and Password: changeme")
 	Add("admin", func(c *Context) error {
 		u := &models.User{Username: "admin", Password: "changeme", PasswordConfirmation: "changeme"}
-		return models.DB.Create(u)
+		_, err := u.Create(models.DB)
+		if err != nil {
+			return err
+		}
+		return nil
 	})
-
 })
