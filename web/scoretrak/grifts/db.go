2e7b3a47065069f3417b3d37ae31966c1df26f9d
l1ghtman2k <aibek.zhil@gmail.com>
2020-04-28 06:51:19 -0400

    Created grift for admin account generation

--- web/scoretrak/grifts/db.go
+++ web/scoretrak/grifts/db.go
@@ -1,15 +1,15 @@
 package grifts
 
 import (
-	"github.com/markbates/grift/grift"
+	"scoretrak/models"
+	. "github.com/markbates/grift/grift"
 )
 
-var _ = grift.Namespace("db", func() {
-
-	grift.Desc("seed", "Seeds a database")
-	grift.Add("seed", func(c *grift.Context) error {
-		// Add DB seeding stuff here
-		return nil
+var _ = Namespace("user", func() {
+	Desc("admin", "Generates admin account with Username: admin, and Password: changeme")
+	Add("admin", func(c *Context) error {
+		u := &models.User{Username: "admin", Password: "changeme", PasswordConfirmation: "changeme"}
+		return models.DB.Create(u)
 	})
 
 })
