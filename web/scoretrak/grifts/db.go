768cc15118819cb3395f2348d21075de0a068391
l1ghtman2k <aibek.zhil@gmail.com>
2020-04-29 05:52:50 -0400

    Renamed namespace for grift task from user to create. Added grift create:admin which creates: - Group "Black Team" - User "admin" who is a member of "black team"

--- web/scoretrak/grifts/db.go
+++ web/scoretrak/grifts/db.go
@@ -3,16 +3,43 @@ package grifts
 import (
 	. "github.com/markbates/grift/grift"
 	"scoretrak/models"
+	"scoretrak/constants"
 	
 )
 
-var _ = Namespace("user", func() {
-	Desc("admin", "Generates admin account with Username: admin, and Password: changeme")
+var _ = Namespace("create", func() {
+	Desc("admin", "Generates admin account and group. Username: admin, Password: changeme")
 	Add("admin", func(c *Context) error {
-		u := &models.User{Username: "admin", Password: "changeme", PasswordConfirmation: "changeme"}
-		_, err := u.Create(models.DB)
+		t := &models.Team{Name: "Black Team", Role: constants.Black}
+		_, err := models.DB.ValidateAndCreate(t)
 		if err != nil {
 			return err
+		}
+		
+		b_t := []models.Team{}
+		query := models.DB.Where("name = 'Black Team'")
+		err = query.All(&b_t)
+		if err != nil {
+			return err
+		}
+		
+		u := &models.User{Username: "admin", Password: "changeme", PasswordConfirmation: "changeme", TeamID: b_t[0].ID}
+		_, err = u.Create(models.DB)
+		if err != nil {
+			return err
+		}
+
+		a_t := []models.User{}
+		query = models.DB.Where("username = 'admin'")
+		err = query.All(&a_t)
+		if err != nil {
+			return err
+		}
+		
+		b_t[0].Users = append(b_t[0].Users, a_t[0])
+		_, err = models.DB.ValidateAndUpdate(&b_t[0])
+		if err != nil {
+		 	return err
 		}
 		return nil
 	})
