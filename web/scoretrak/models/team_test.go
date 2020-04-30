a6b1c7cdf1536033d8db8a0744c334f9216c24c2
l1ghtman2k <aibek.zhil@gmail.com>
2020-04-30 06:48:28 -0400

    Created a fixture, and tests for user, and team models.

--- web/scoretrak/models/team_test.go
+++ web/scoretrak/models/team_test.go
@@ -1,5 +1,67 @@
 package models
 
-func (ms *ModelSuite) Test_Team() {
-	ms.Fail("This test needs to be implemented!")
+func (ms *ModelSuite) Test_Team_Create() {
+	count, err := ms.DB.Count("teams")
+	ms.NoError(err)
+	ms.Equal(0, count)
+
+	t := &Team{
+		Name: "TestName",
+		Role: "blue",
+	}
+	verrs, err := ms.DB.ValidateAndCreate(t)
+	ms.NoError(err)
+	ms.False(verrs.HasAny())
+	count, err = ms.DB.Count("teams")
+	ms.NoError(err)
+	ms.Equal(1, count)
+}
+
+func (ms *ModelSuite) Test_Team_Create_ValidationErrors() {
+	count, err := ms.DB.Count("teams")
+	ms.NoError(err)
+	ms.Equal(0, count)
+
+	t := &Team{
+		Name: "TestName",
+		Role: "nonexistent",
+	}
+
+	verrs, err := ms.DB.ValidateAndCreate(t)
+	ms.NoError(err)
+	ms.True(verrs.HasAny())
+
+	count, err = ms.DB.Count("teams")
+	ms.NoError(err)
+	ms.Equal(0, count)
+}
+
+func (ms *ModelSuite) Test_Team_Create_TeamExists() {
+	count, err := ms.DB.Count("teams")
+	ms.NoError(err)
+	ms.Equal(0, count)
+
+	t := &Team{
+		Name: "DuplicateName",
+		Role: "black",
+	}
+
+	verrs, err := ms.DB.ValidateAndCreate(t)
+	ms.NoError(err)
+	ms.False(verrs.HasAny())
+	count, err = ms.DB.Count("teams")
+	ms.NoError(err)
+	ms.Equal(1, count)
+
+	t = &Team{
+		Name: "DuplicateName",
+		Role: "red",
+	}
+
+	verrs, err = ms.DB.ValidateAndCreate(t)
+	ms.NoError(err)
+	ms.True(verrs.HasAny())
+	count, err = ms.DB.Count("teams")
+	ms.NoError(err)
+	ms.Equal(1, count)
 }
--- web/scoretrak/models/user_test.go
+++ web/scoretrak/models/user_test.go
@@ -1,15 +1,27 @@
 package models
 
+import (
+	"log"
+	"github.com/gofrs/uuid"
+)
+
 func (ms *ModelSuite) Test_User_Create() {
+	
+	ms.LoadFixture("Create a Team")
+	t, terr := get_Team(ms)
+	if terr != nil{
+		log.Fatal(terr)
+	}
+	u := &User{
+		Username:             "testuser",
+		Password:             "testpass",
+		PasswordConfirmation: "testpass",
+		TeamID:				  t.ID,
+	}
+
 	count, err := ms.DB.Count("users")
 	ms.NoError(err)
 	ms.Equal(0, count)
-
-	u := &User{
-		Username:             "mark@example.com",
-		Password:             "password",
-		PasswordConfirmation: "password",
-	}
 
 	ms.Zero(u.PasswordHash)
 
@@ -21,16 +33,19 @@ func (ms *ModelSuite) Test_User_Create() {
 	count, err = ms.DB.Count("users")
 	ms.NoError(err)
 	ms.Equal(1, count)
+
+	destroy_Users(ms)
 }
 
 func (ms *ModelSuite) Test_User_Create_ValidationErrors() {
-	count, err := ms.DB.Count("users")
-	ms.NoError(err)
-	ms.Equal(0, count)
 
 	u := &User{
 		Password: "password",
 	}
+
+	count, err := ms.DB.Count("users")
+	ms.NoError(err)
+	ms.Equal(0, count)
 
 	ms.Zero(u.PasswordHash)
 
@@ -41,18 +56,25 @@ func (ms *ModelSuite) Test_User_Create_ValidationErrors() {
 	count, err = ms.DB.Count("users")
 	ms.NoError(err)
 	ms.Equal(0, count)
+
 }
 
 func (ms *ModelSuite) Test_User_Create_UserExists() {
+	ms.LoadFixture("Create a Team")
+	t, terr := get_Team(ms)
+	if terr != nil{
+		log.Fatal(terr)
+	}
+	u := &User{
+		Username:             "testuser",
+		Password:             "testpass",
+		PasswordConfirmation: "testpass",
+		TeamID:				  t.ID,
+	}
+
 	count, err := ms.DB.Count("users")
 	ms.NoError(err)
 	ms.Equal(0, count)
-
-	u := &User{
-		Username:             "mark@example.com",
-		Password:             "password",
-		PasswordConfirmation: "password",
-	}
 
 	ms.Zero(u.PasswordHash)
 
@@ -66,8 +88,10 @@ func (ms *ModelSuite) Test_User_Create_UserExists() {
 	ms.Equal(1, count)
 
 	u = &User{
-		Username: "mark@example.com",
-		Password: "password",
+		Username:             "testuser",
+		Password:             "testpass",
+		PasswordConfirmation: "testpass",
+		TeamID:				  t.ID,
 	}
 
 	verrs, err = u.Create(ms.DB)
@@ -77,4 +101,56 @@ func (ms *ModelSuite) Test_User_Create_UserExists() {
 	count, err = ms.DB.Count("users")
 	ms.NoError(err)
 	ms.Equal(1, count)
+	destroy_Users(ms)
 }
+
+
+func (ms *ModelSuite) Test_User_Create_NonExistentTeamID() {
+
+	s := "6ba7b810-9dad-11d1-80b4-00c04fd430c8" //Non-existent UUID.
+	u3, err := uuid.FromString(s)
+	if err != nil {
+		log.Fatalf("failed to parse UUID %q: %v", s, err)
+	}
+
+	count, err := ms.DB.Count("users")
+	ms.NoError(err)
+	ms.Equal(0, count)
+
+	u := &User{
+		Username:             "mark@example.com",
+		Password:             "password",
+		PasswordConfirmation: "password",
+		TeamID:				  u3,
+	}
+
+	ms.Zero(u.PasswordHash)
+	verrs, err := u.Create(ms.DB)
+	ms.NoError(err)
+	ms.True(verrs.HasAny())
+}
+
+
+func get_Team(ms *ModelSuite) (Team, error){
+	b_t := []Team{}
+	query := ms.DB.Where("name = 'Mock Team'")
+	err := query.All(&b_t)
+	if err != nil {
+		return Team{}, err
+	}
+
+	return b_t[0], err
+}
+
+func destroy_Users(ms *ModelSuite){
+	users := []User{}
+	err := ms.DB.All(&users)
+	if err != nil{
+		panic(err)
+	}
+	for i := 0; i < len(users); i++ {
+		user := users[i]
+		ms.DB.Destroy(&user)
+	}
+}
+
