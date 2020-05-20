e3342894b25ec3f472f2cb18ea383dac8e5f35e3
l1ghtman2k <aibek.zhil@gmail.com>
2020-05-20 18:58:30 -0400

    Adjusting API for swarm and config

--- pkg/storage/orm/config_repo.go
+++ pkg/storage/orm/config_repo.go
@@ -17,12 +17,13 @@ func NewConfigRepo(db *gorm.DB, log logger.LogInfoFormat) config.Repo {
 
 func (c *configRepo) Get() (*config.DynamicConfig, error) {
 	cfg := &config.DynamicConfig{}
+	c.db.Take(&cfg)
 	return cfg, nil
 }
 
 func (c *configRepo) Update(cfg *config.DynamicConfig) error {
-	c.log.Debugf("updating the config, config_id")
-	err := c.db.Model(&cfg).Updates(config.DynamicConfig{}).Error
+	c.log.Debugf("updating the config")
+	err := c.db.Model(&cfg).Updates(config.DynamicConfig{RoundDuration: cfg.RoundDuration, Enabled: cfg.Enabled}).Error
 	if err != nil {
 		c.log.Errorf("error while updating the config, reason : %v", err)
 		return err
