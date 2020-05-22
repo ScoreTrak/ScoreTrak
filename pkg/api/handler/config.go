e911045580ac897673c986f31ac9408703e5e0c5
l1ghtman2k <aibek.zhil@gmail.com>
2020-05-21 21:04:13 -0400

    Configured API for config and host models

--- pkg/api/handler/config.go
+++ pkg/api/handler/config.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"ScoreTrak/pkg/config"
 	"ScoreTrak/pkg/logger"
+	"encoding/json"
 	"net/http"
 )
 
@@ -17,10 +18,35 @@ func NewConfigController(log logger.LogInfoFormat, svc config.Serv) *configContr
 
 func (c *configController) Update(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	decoder := json.NewDecoder(r.Body)
+	var sg config.DynamicConfig
+	err := decoder.Decode(&sg)
+	if err != nil {
+		c.log.Error(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = c.svc.Update(&sg)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 	w.WriteHeader(http.StatusOK)
 }
 
 func (c *configController) Get(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
+	conf, err := c.svc.Get()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		c.log.Error(err)
+		return
+	}
+	encoder := json.NewEncoder(w)
+	err = encoder.Encode(conf)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		c.log.Error(err)
+	}
+
 }
