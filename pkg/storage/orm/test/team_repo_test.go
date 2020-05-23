29aeac752fadfeca16d5b890422530e8793ea6e1
l1ghtman2k <aibek.zhil@gmail.com>
2020-05-23 13:51:04 -0400

    More tests

--- pkg/storage/orm/test/team_repo_test.go
+++ pkg/storage/orm/test/team_repo_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"ScoreTrak/pkg/storage/orm"
+	"ScoreTrak/pkg/team"
 	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 )
@@ -20,12 +21,25 @@ func TestSpec(t *testing.T) {
 			cleanDB(db)
 		})
 		Convey("When the database is empty", func() {
+			tr = orm.NewTeamRepo(db, l)
 			Convey("There should be no entries", func() {
-				tr = orm.NewTeamRepo(db, l)
 				ac, err := tr.GetAll()
 				So(err, ShouldBeNil)
 				So(len(ac), ShouldEqual, 0)
 			})
+
+			Convey("Adding an entry", func() {
+				var err error
+				t := team.Team{ID: "TestTeam"}
+				err = tr.Store(&t)
+				So(err, ShouldBeNil)
+				Convey("Should output one entry", func() {
+					ac, err := tr.GetAll()
+					So(err, ShouldBeNil)
+					So(len(ac), ShouldEqual, 1)
+				})
+			})
+
 		})
 	})
 
