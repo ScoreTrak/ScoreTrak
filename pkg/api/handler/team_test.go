4474106d500c7482b2881d17436043e6a5154672
l1ghtman2k <aibek.zhil@gmail.com>
2020-05-29 20:03:43 -0400

    Sample test for API team service

---
+++ pkg/api/handler/team_test.go
@@ -0,0 +1,27 @@
+package handler
+
+import (
+	"ScoreTrak/pkg/storage/orm"
+	. "ScoreTrak/test"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestCheckSpec(t *testing.T) {
+	c := NewConfigClone(SetupConfig("dev-config.yml"))
+	c.DB.Cockroach.Database = "scoretrak_test_api_team"
+	c.Logger.FileName = "service_group_test_repo.log"
+	db := SetupDB(c)
+	l := SetupLogger(c)
+	DataPreload(db)
+	t.Parallel() //t.Parallel should be placed after SetupDB because gorm has race conditions on Hook register
+	Convey("Initializing check repo and controller", t, func() {
+		cr := orm.NewTeamRepo(db, l)
+		_ = NewTeamController(l, cr)
+		Convey("", func() {
+
+		})
+	})
+	DropDB(db, c)
+	db.Close()
+}
