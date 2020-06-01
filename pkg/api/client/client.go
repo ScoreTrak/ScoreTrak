83b88d4692250ab2fc8bdb8aabf14813cbfa542e
l1ghtman2k <aibek.zhil@gmail.com>
2020-06-01 00:22:07 -0400

    Simple comment/reminder for future me

--- pkg/api/client/client.go
+++ pkg/api/client/client.go
@@ -14,3 +14,5 @@ type ScoretrakClient struct {
 func NewScoretrakClient(address, port, token string, client *http.Client) ScoretrakClient {
 	return ScoretrakClient{address: address, port: port, token: token, httpClient: client}
 }
+
+//Every Client could be fed directly to resource struct in gobuffalo
