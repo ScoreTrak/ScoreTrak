e34b5895c40e8f6f0bdbcbd72b19ad9474ec9985
l1ghtman2k <aibek.zhil@gmail.com>
2020-06-02 16:35:54 -0400

    Change Address and port to url.URL. Implemented responseValidator (as per https://medium.com/@marcus.olsson/writing-a-go-client-for-your-restful-api-c193a2f4998c)
    Implemented team client.go

--- pkg/api/client/client.go
+++ pkg/api/client/client.go
@@ -1,18 +1,33 @@
 package client
 
 import (
+	"fmt"
 	"net/http"
+	"net/url"
 )
 
 type ScoretrakClient struct {
-	address    string
-	port       string
+	BaseURL    *url.URL
 	token      string
 	httpClient *http.Client
 }
 
-func NewScoretrakClient(address, port, token string, client *http.Client) ScoretrakClient {
-	return ScoretrakClient{address: address, port: port, token: token, httpClient: client}
+func NewScoretrakClient(url *url.URL, token string, client *http.Client) ScoretrakClient {
+	return ScoretrakClient{BaseURL: url, token: token, httpClient: client}
 }
 
 //Every Client could be fed directly to resource struct in gobuffalo
+
+func responseValidator(r *http.Response) error {
+	if r.StatusCode >= 200 && r.StatusCode < 400 {
+		return nil
+	}
+	return &InvalidResponse{fmt.Sprintf("Invalid response code received: %d", r.StatusCode)}
+
+}
+
+type InvalidResponse struct {
+	msg string // description of error
+}
+
+func (e *InvalidResponse) Error() string { return e.msg }
--- pkg/api/client/team.go
+++ pkg/api/client/team.go
@@ -1,6 +1,13 @@
 package client
 
-import "ScoreTrak/pkg/team"
+import (
+	"ScoreTrak/pkg/team"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
 
 type teamClient struct {
 	s ScoretrakClient
@@ -11,21 +18,106 @@ func NewTeamClient(c ScoretrakClient) team.Serv {
 }
 
 func (t teamClient) Delete(id string) error {
-	panic("implement me")
+	rel := &url.URL{Path: fmt.Sprintf("/team/%s", id)}
+	u := t.s.BaseURL.ResolveReference(rel)
+	req, err := http.NewRequest("DELETE", u.String(), nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Accept", "application/json")
+	resp, err := t.s.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	return err
 }
 
 func (t teamClient) GetAll() ([]*team.Team, error) {
-	panic("implement me")
+	rel := &url.URL{Path: "/team"}
+	u := t.s.BaseURL.ResolveReference(rel)
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Accept", "application/json")
+	resp, err := t.s.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	err = responseValidator(resp)
+	if err != nil {
+		return nil, err
+	}
+	var teams []*team.Team
+	err = json.NewDecoder(resp.Body).Decode(&teams)
+	return teams, err
 }
 
 func (t teamClient) GetByID(id string) (*team.Team, error) {
-	panic("implement me")
+	rel := &url.URL{Path: fmt.Sprintf("/team/%s", id)}
+	u := t.s.BaseURL.ResolveReference(rel)
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Accept", "application/json")
+	resp, err := t.s.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var tm *team.Team
+	err = responseValidator(resp)
+	if err != nil {
+		return nil, err
+	}
+	err = json.NewDecoder(resp.Body).Decode(&tm)
+	if err != nil {
+		return nil, err
+	}
+	return tm, nil
 }
 
-func (t teamClient) Store(u *team.Team) error {
-	panic("implement me")
+func (t teamClient) Store(ut *team.Team) error {
+	rel := &url.URL{Path: fmt.Sprintf("/team/%s", ut.ID)}
+	u := t.s.BaseURL.ResolveReference(rel)
+	e, err := json.Marshal(ut)
+	if err != nil {
+		return nil
+	}
+	b := bytes.NewBuffer(e)
+	req, err := http.NewRequest("POST", u.String(), b)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Accept", "application/json")
+	resp, err := t.s.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	return responseValidator(resp)
 }
 
-func (t teamClient) Update(u *team.Team) error {
-	panic("implement me")
+func (t teamClient) Update(ut *team.Team) error {
+	rel := &url.URL{Path: fmt.Sprintf("/team/%s", ut.ID)}
+	u := t.s.BaseURL.ResolveReference(rel)
+	e, err := json.Marshal(ut)
+	if err != nil {
+		return nil
+	}
+	b := bytes.NewBuffer(e)
+	req, err := http.NewRequest("PATCH", u.String(), b)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Accept", "application/json")
+	resp, err := t.s.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	return responseValidator(resp)
 }
