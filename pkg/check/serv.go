8c2bfb2315dd12adae99836738eda59283161dc0
l1ghtman2k <aibek.zhil@gmail.com>
2020-06-03 12:56:09 -0400

    Change Serv for check and round so they do not bind optional api methods

--- pkg/check/serv.go
+++ pkg/check/serv.go
@@ -3,11 +3,6 @@ package check
 type Serv interface {
 	GetAllByRoundID(rID uint64) ([]*Check, error)
 	GetByRoundServiceID(rID uint64, sID uint64) ([]*Check, error)
-	Delete(id uint64) error
-	GetAll() ([]*Check, error)
-	GetByID(id uint64) (*Check, error)
-	Store(u *Check) error
-	StoreMany(u []*Check) error
 }
 
 type checkServ struct {
