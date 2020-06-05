a8a181475c5720361e04d416cf26747c6e1fe623
l1ghtman2k <aibek.zhil@gmail.com>
2020-06-05 01:15:46 -0400

    Replace Config update method with a manual representation of the code. Reason: No id specified on update.

--- pkg/api/handler/config.go
+++ pkg/api/handler/config.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"ScoreTrak/pkg/config"
 	"ScoreTrak/pkg/logger"
+	"encoding/json"
+	"github.com/qor/validations"
 	"net/http"
 )
 
@@ -17,10 +19,26 @@ func NewConfigController(log logger.LogInfoFormat, svc config.Serv) *configContr
 
 func (c *configController) Update(w http.ResponseWriter, r *http.Request) {
 	sg := &config.DynamicConfig{}
-	genericUpdate(c.svc, sg, c.log, "Update", w, r)
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(sg)
+	if err != nil {
+		c.log.Error(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = c.svc.Update(sg)
+	if err != nil {
+		_, ok := err.(*validations.Error)
+		if ok {
+			w.WriteHeader(http.StatusPreconditionFailed)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		c.log.Error(err)
+		return
+	}
 }
 
 func (c *configController) Get(w http.ResponseWriter, r *http.Request) {
 	genericGet(c.svc, c.log, "Get", w, r)
-
 }
