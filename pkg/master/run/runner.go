3b0d17b2d0a9ecf5b42d20328334a4c792740884
l1ghtman2k <aibek.zhil@gmail.com>
2020-06-12 00:34:06 -0400

    Add the logic to attempt to start the round

--- pkg/master/run/runner.go
+++ pkg/master/run/runner.go
@@ -6,6 +6,7 @@ import (
 	"ScoreTrak/pkg/round"
 	"ScoreTrak/pkg/storage/orm"
 	"github.com/jinzhu/gorm"
+	"github.com/lib/pq"
 	"time"
 )
 
@@ -20,7 +21,7 @@ func NewRunner(db *gorm.DB, l logger.LogInfoFormat) *drunner {
 	}
 }
 
-func (d drunner) MasterRunner(db *gorm.DB, l logger.LogInfoFormat) error {
+func (d *drunner) MasterRunner(db *gorm.DB, l logger.LogInfoFormat) error {
 	configLoop := time.NewTicker(config.MinRoundDuration)
 	cnfRepo := orm.NewConfigRepo(db, l)
 	var scoringLoop *time.Ticker
@@ -29,7 +30,7 @@ func (d drunner) MasterRunner(db *gorm.DB, l logger.LogInfoFormat) error {
 	if err != nil {
 		rnd, err = rndRepo.GetLastRound()
 		if err != nil {
-			rnd = d.attemptToScore()
+			rnd = d.attemptToScore(nil)
 		}
 	}
 	scoringLoop = time.NewTicker(d.durationUntilNextRound(rnd))
@@ -40,7 +41,7 @@ func (d drunner) MasterRunner(db *gorm.DB, l logger.LogInfoFormat) error {
 			cnf, _ := cnfRepo.Get()
 			if !config.IsEqual(cnf) {
 				config.UpdateConfig(cnf)
-				rnd, err := rndRepo.GetElapsingRound()
+				rnd, err = rndRepo.GetElapsingRound()
 				if err != nil {
 					rnd, err = rndRepo.GetLastRound()
 					if err != nil {
@@ -51,7 +52,7 @@ func (d drunner) MasterRunner(db *gorm.DB, l logger.LogInfoFormat) error {
 				scoringLoop = time.NewTicker(d.durationUntilNextRound(rnd))
 			}
 		case <-scoringLoop.C:
-			rnd = d.attemptToScore()
+			rnd = d.attemptToScore(rnd)
 			scoringLoop.Stop()
 			scoringLoop = time.NewTicker(d.durationUntilNextRound(rnd))
 		}
@@ -59,7 +60,7 @@ func (d drunner) MasterRunner(db *gorm.DB, l logger.LogInfoFormat) error {
 	}
 }
 
-func (d drunner) durationUntilNextRound(rnd *round.Round) time.Duration {
+func (d *drunner) durationUntilNextRound(rnd *round.Round) time.Duration {
 	if rnd == nil {
 		return config.MinRoundDuration
 	}
@@ -71,11 +72,31 @@ func (d drunner) durationUntilNextRound(rnd *round.Round) time.Duration {
 }
 
 //Runs check in the background as a gorutine.
-func (d drunner) attemptToScore() (elapsingRound *round.Round) {
-	return nil
+func (d *drunner) attemptToScore(rnd *round.Round) (elapsingRound *round.Round) {
+	if !config.GetEnabled() {
+		return nil
+	}
+	r := round.Round{}
+	if rnd == nil {
+		r.ID = 1
+	} else {
+		r.ID = rnd.ID + 1
+	}
+	rndRepo := orm.NewRoundRepo(d.db, d.l)
+	err := rndRepo.Store(&r)
+	if err != nil {
+		serr, ok := err.(*pq.Error)
+		if ok && serr.Code.Name() == "unique_violation" {
+			return &r
+		}
+		d.l.Error(err)
+		panic(err)
+	}
+	go d.Score(r)
+	return &r
 }
 
-//AttemptToScore should return last round that is being scored
-//main loop should reassign/normalize timer based on AttemptToScoreAnswer, and round duration
-
-//CREATE CLEAR TIMELINE
+func (d drunner) Score(rnd round.Round) {
+	//Perform Scoring
+	//Save
+}
