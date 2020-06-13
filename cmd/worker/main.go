1c0e33b026c93099cb8d09d093944fbfdff2d8fe
l1ghtman2k <aibek.zhil@gmail.com>
2020-06-13 18:57:23 -0400

    Added Worker outline

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,1 +1,20 @@
-package worker
+package main
+
+import (
+	"ScoreTrak/pkg/config"
+	"ScoreTrak/pkg/queue"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if err := config.NewStaticConfig("configs/config.yml"); err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(-1)
+	}
+	q, err := queue.NewQueue(config.GetConfig())
+	if err != nil {
+		panic(err)
+	}
+	q.Receive()
+}
