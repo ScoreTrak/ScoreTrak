4b156309f45089c8db4d29f1bef420c2de671178
l1ghtman2k <aibek.zhil@gmail.com>
2020-06-16 17:14:01 -0400

    modified LDAP, and Winrm to make the choices more user friendly by assigning default values

--- pkg/exec/services/http.go
+++ pkg/exec/services/http.go
@@ -12,14 +12,14 @@ import (
 
 type HTTP struct {
 	ExpectedOutput string `json:"text"`
-	Protocol       string `json:"protocol"`
+	Scheme         string `json:"scheme"`
 	Port           string `json:"port"`
 	Path           string `json:"path"`
 	Subdomain      string `json:"subdomain"`
 }
 
 func NewHTTP() *HTTP {
-	f := HTTP{Port: "80", Protocol: "http"}
+	f := HTTP{Scheme: "http"}
 	return &f
 }
 
@@ -28,11 +28,18 @@ func (h *HTTP) Validate() error {
 }
 
 func (h *HTTP) Execute(e exec.Exec) (passed bool, log string, err error) {
+	if h.Port == "" {
+		if strings.ToLower(h.Scheme) == "https" {
+			h.Port = "443"
+		} else {
+			h.Port = "80"
+		}
+	}
 	if h.Subdomain != "" && h.Subdomain[len(h.Subdomain)-1:] != "." {
 		h.Subdomain += "."
 	}
 	host := h.Subdomain + e.Host + ":" + h.Port
-	baseURL := url.URL{Path: h.Path, Scheme: h.Protocol, Host: host}
+	baseURL := url.URL{Path: h.Path, Scheme: h.Scheme, Host: host}
 	req, err := http.NewRequest("GET", baseURL.String(), nil)
 	if err != nil {
 		return false, "Error while crafting the request", err
@@ -50,7 +57,10 @@ func (h *HTTP) Execute(e exec.Exec) (passed bool, log string, err error) {
 	}
 	if h.ExpectedOutput != "" {
 		buf := new(bytes.Buffer)
-		buf.ReadFrom(resp.Body)
+		_, err = buf.ReadFrom(resp.Body)
+		if err != nil {
+			return false, "Unable to read response body", err
+		}
 		newStr := buf.String()
 		if !strings.Contains(newStr, h.ExpectedOutput) {
 			return false, "the page output doesn't contain expected output", nil
--- pkg/exec/services/winrm.go
+++ pkg/exec/services/winrm.go
@@ -15,11 +15,11 @@ type Winrm struct {
 	Port           string `json:"port"`
 	Command        string `json:"command"`
 	ExpectedOutput string `json:"expected_output"`
-	Protocol       string `json:"protocol"`
+	Scheme         string `json:"scheme"`
 }
 
 func NewWinrm() *Winrm {
-	f := Winrm{Port: "5985", Command: "whoami", Protocol: "http"}
+	f := Winrm{Command: "whoami", Scheme: "http"}
 	return &f
 }
 
@@ -32,8 +32,15 @@ func (w *Winrm) Validate() error {
 
 func (w *Winrm) Execute(e exec.Exec) (passed bool, log string, err error) {
 	var isHttps bool
-	if strings.ToLower(w.Protocol) == "https" {
+	if strings.ToLower(w.Scheme) == "https" {
 		isHttps = true
+	}
+	if w.Port == "" {
+		if isHttps {
+			w.Port = "5986"
+		} else {
+			w.Port = "5985"
+		}
 	}
 	i, err := strconv.Atoi(w.Port)
 	if err != nil {
