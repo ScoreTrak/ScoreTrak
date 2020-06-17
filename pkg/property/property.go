2666e2caa56a4e5cffd710094ee5ad05b9aa30de
l1ghtman2k <aibek.zhil@gmail.com>
2020-06-17 01:17:36 -0400

    Removed validations for properties

--- pkg/property/property.go
+++ pkg/property/property.go
@@ -28,30 +28,10 @@ func (p Property) Validate(db *gorm.DB) {
 	if p.Status != "" {
 		for _, item := range []string{"View", "Edit", "Hide"} {
 			if item == p.Status {
-				goto NextCheck
+				return
 			}
 		}
 		db.AddError(errors.New("property Status should either be View, Edit, or Hide"))
 		return
 	}
-
-NextCheck:
-	if p.Value != "" {
-		var status string
-		if p.Status == "" {
-			pe := Property{}
-			db.Where("id = ?", p.ID).First(&pe)
-			if pe.Status == "" {
-				status = "View"
-			} else {
-				status = pe.Status
-			}
-		} else {
-			status = p.Status
-		}
-		if status != "Edit" {
-			db.AddError(errors.New("you cannot edit value if Status is not Edit"))
-		}
-	}
-	return
 }
