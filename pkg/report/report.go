38845e4144d963cf8e2c71e858900937b35bea09
l1ghtman2k <aibek.zhil@gmail.com>
2020-06-20 20:34:25 -0400

    Added ability to recalculate the report

--- pkg/report/report.go
+++ pkg/report/report.go
@@ -1,6 +1,10 @@
 package report
 
-import "time"
+import (
+	"ScoreTrak/pkg/round"
+	"ScoreTrak/pkg/team"
+	"time"
+)
 
 type Report struct {
 	ID        uint64    `json:"id,omitempty"`
@@ -17,6 +21,43 @@ func NewReport() *Report {
 	c.ID = 1
 	c.Cache = "{}"
 	return c
+}
+
+func RecalculateReport(team []*team.Team, round round.Round) (simpleTeams map[uint64]SimpleTeam, err error) {
+	simpleTeams = make(map[uint64]SimpleTeam)
+	for _, t := range team {
+		st := SimpleTeam{}
+		st.Hosts = make(map[uint64]SimpleHost)
+		for _, h := range t.Hosts {
+			sh := SimpleHost{}
+			sh.Services = make(map[uint64]SimpleService)
+			for _, s := range h.Services {
+				var points uint64
+				for _, c := range s.Checks {
+					if *c.Passed {
+						points += s.Points
+					}
+				}
+				params := map[string]string{}
+				for _, p := range s.Properties {
+					if p.Status != "Hide" {
+						params[p.Key] = p.Value
+					}
+				}
+				if len(s.Checks) != 0 {
+					lastCheck := s.Checks[len(s.Checks)-1]
+					if lastCheck.RoundID == round.ID {
+						sh.Services[s.ID] = SimpleService{Passed: *lastCheck.Passed, Log: lastCheck.Log, Err: lastCheck.Err, Points: points, Properties: params, PointsBoost: s.PointsBoost}
+					} else {
+						sh.Services[s.ID] = SimpleService{Passed: false, Log: "Service was not checked because it was disabled", Err: "", Points: points, Properties: params, PointsBoost: s.PointsBoost}
+					}
+				}
+			}
+			st.Hosts[h.ID] = sh
+		}
+		simpleTeams[t.ID] = st
+	}
+	return simpleTeams, nil
 }
 
 type SimpleReport struct {
