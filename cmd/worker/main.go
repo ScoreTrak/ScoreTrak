e3c00699a0d5a5a5e04bcebd219490c23db15b32
l1ghtman2k <aibek.zhil@gmail.com>
2020-06-22 20:34:00 -0400

    Added flags to pass config path to master, and worker

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/L1ghtman2k/ScoreTrak/pkg/config"
 	"github.com/L1ghtman2k/ScoreTrak/pkg/logger"
@@ -9,7 +10,9 @@ import (
 )
 
 func main() {
-	if err := config.NewStaticConfig("configs/config.yml"); err != nil {
+	path := flag.String("config-file-path", "configs/config.yml", "Please enter a path to config file")
+	flag.Parse()
+	if err := config.NewStaticConfig(*path); err != nil {
 		fmt.Fprintf(os.Stderr, "%v", err)
 		os.Exit(-1)
 	}
