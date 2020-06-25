da512c976c0e6daf661849aaffaa35395ec9b8e8
l1ghtman2k <aibek.zhil@gmail.com>
2020-06-25 04:50:49 -0400

    Add flags to pass config for cmd

--- cmd/master/main.go
+++ cmd/master/main.go
@@ -9,13 +9,20 @@ import (
 )
 
 func main() {
-	path := flag.String("config-file-path", "configs/config.yml", "Please enter a path to config file")
+	path := flag.String("config", "configs/config.yml", "Please enter a path to config file")
 	flag.Parse()
+	if *path != "configs/config.yml" {
+		_, err := os.Stat(*path)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Provided config was not found")
+			os.Exit(-1)
+		}
+	}
 	if err := config.NewStaticConfig(*path); err != nil {
 		fmt.Fprintf(os.Stderr, "%v", err)
 		os.Exit(-1)
 	}
-
+	fmt.Println(*path)
 	if err := master.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to start due to: %v", err)
 		os.Exit(-1)
--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -10,8 +10,15 @@ import (
 )
 
 func main() {
-	path := flag.String("config-file-path", "configs/config.yml", "Please enter a path to config file")
+	path := flag.String("config", "configs/config.yml", "Please enter a path to config file")
 	flag.Parse()
+	if *path != "configs/config.yml" {
+		_, err := os.Stat(*path)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Provided config was not found")
+			os.Exit(-1)
+		}
+	}
 	if err := config.NewStaticConfig(*path); err != nil {
 		fmt.Fprintf(os.Stderr, "%v", err)
 		os.Exit(-1)
@@ -21,7 +28,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	q, err := queue.NewQueue(config.GetConfig(), l)
+	q, err := queue.NewQueue(config.GetStaticConfig(), l)
 	if err != nil {
 		panic(err)
 	}
