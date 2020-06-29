adf836e716044cf7fa7cf7c7a828bedf7d537b30
l1ghtman2k <aibek.zhil@gmail.com>
2020-06-29 19:36:21 -0400

    Added ability to take config via encoded string

--- cmd/master/main.go
+++ cmd/master/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"flag"
 	"fmt"
 	"github.com/L1ghtman2k/ScoreTrak/pkg/config"
@@ -10,11 +11,35 @@ import (
 
 func main() {
 	path := flag.String("config", "configs/config.yml", "Please enter a path to config file")
+	encodedConfig := flag.String("encoded-config", "", "Please enter encoded config")
 	flag.Parse()
-	if !configExists(*path) {
+	if *encodedConfig != "" {
+		dec, err := base64.StdEncoding.DecodeString(*encodedConfig)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v", err)
+			os.Exit(-1)
+		}
+		*path = "config.yml"
+		f, err := os.Create(*path)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v", err)
+			os.Exit(-1)
+		}
+		defer f.Close()
+
+		if _, err := f.Write(dec); err != nil {
+			fmt.Fprintf(os.Stderr, "%v", err)
+			os.Exit(-1)
+		}
+		if err := f.Sync(); err != nil {
+			fmt.Fprintf(os.Stderr, "%v", err)
+			os.Exit(-1)
+		}
+	} else if !configExists(*path) {
 		fmt.Fprintf(os.Stderr, "You need to provide config!")
 		os.Exit(-1)
 	}
+
 	if err := config.NewStaticConfig(*path); err != nil {
 		fmt.Fprintf(os.Stderr, "%v", err)
 		os.Exit(-1)
