6d151e560e607de10e9ec44399955042571c8d22
l1ghtman2k <aibek.zhil@gmail.com>
2020-07-01 01:11:19 -0400

    removed a note

--- pkg/service_group/service_group.go
+++ pkg/service_group/service_group.go
@@ -26,7 +26,6 @@ func (ServiceGroup) TableName() string {
 	return "service_groups"
 }
 
-//Todo: Place validatinos on name to ensure queues can use ServiceGroup name without a problem.
 func (s ServiceGroup) Validate(db *gorm.DB) {
 	if s.Name != "" { //https://github.com/nsqio/go-nsq/blob/04552936c57a26026c39e10a8993805e0f5a73d0/protocol.go
 		if len(s.Name) > 1 && len(s.Name) <= 64 && regexp.MustCompile(`^[\.a-zA-Z0-9_-]+(#ephemeral)?$`).MatchString(s.Name) {
