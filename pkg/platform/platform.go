cb5f078e0d0597867dcd735bf7c7875bf51c7a99
l1ghtman2k <aibek.zhil@gmail.com>
2020-07-12 05:51:25 -0400

    Added boilerplate kubernetes platform module

--- pkg/platform/platform.go
+++ pkg/platform/platform.go
@@ -5,6 +5,7 @@ import (
 	"github.com/L1ghtman2k/ScoreTrak/pkg/config"
 	"github.com/L1ghtman2k/ScoreTrak/pkg/logger"
 	"github.com/L1ghtman2k/ScoreTrak/pkg/platform/docker"
+	"github.com/L1ghtman2k/ScoreTrak/pkg/platform/kubernetes"
 	"github.com/L1ghtman2k/ScoreTrak/pkg/platform/worker"
 )
 
@@ -16,6 +17,8 @@ type Platform interface {
 func NewPlatform(config config.StaticConfig, l logger.LogInfoFormat) (Platform, error) {
 	if config.Platform.Use == "docker" || config.Platform.Use == "swarm" {
 		return docker.NewDocker(config, l)
+	} else if config.Platform.Use == "kubernetes" {
+		return kubernetes.NewKubernetes(config, l)
 	} else if config.Platform.Use == "none" {
 		return nil, nil
 	}
