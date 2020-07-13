e95eb039011d1a084f62c0ed92a004dbd4aeb177
l1ghtman2k <aibek.zhil@gmail.com>
2020-07-13 04:48:07 -0400

    Moved DB and Logger struct declarations outside of struct definition

--- pkg/config/staticconf.go
+++ pkg/config/staticconf.go
@@ -12,29 +12,9 @@ type StaticConfig struct {
 	// token specified on init of the staticConfig
 	Token string `default:""`
 
-	DB struct {
-		Use       string `default:"cockroach"`
-		Cockroach struct {
-			Enabled           bool   `default:"true"`
-			Host              string `default:"cockroach"`
-			Port              string `default:"26257"`
-			UserName          string `default:"root"`
-			Password          string `default:""`
-			Database          string `default:"scoretrak"`
-			ConfigureZones    bool   `default:"true"`
-			DefaultZoneConfig struct {
-				GcTtlseconds                    uint64 `default:"600"`
-				BackpressureRangeSizeMultiplier uint64 `default:"0"`
-			}
-		}
-	}
+	DB DB
 
-	Logger struct {
-		Use         string `default:"zapLogger"`
-		Environment string `default:"prod"`
-		LogLevel    string `default:"info"`
-		FileName    string `default:"scoretrak.log"`
-	}
+	Logger Logger
 
 	Queue struct {
 		Use   string `default:"none"`
@@ -68,6 +48,30 @@ type StaticConfig struct {
 			Namespace string `default:"default"`
 		}
 	}
+}
+
+type DB struct {
+	Use       string `default:"cockroach"`
+	Cockroach struct {
+		Enabled           bool   `default:"true"`
+		Host              string `default:"cockroach"`
+		Port              string `default:"26257"`
+		UserName          string `default:"root"`
+		Password          string `default:""`
+		Database          string `default:"scoretrak"`
+		ConfigureZones    bool   `default:"true"`
+		DefaultZoneConfig struct {
+			GcTtlseconds                    uint64 `default:"600"`
+			BackpressureRangeSizeMultiplier uint64 `default:"0"`
+		}
+	}
+}
+
+type Logger struct {
+	Use         string `default:"zapLogger"`
+	Environment string `default:"prod"`
+	LogLevel    string `default:"info"`
+	FileName    string `default:"scoretrak.log"`
 }
 
 var staticConfig StaticConfig
