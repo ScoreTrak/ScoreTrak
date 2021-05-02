095716f36cef2bf086634a803ab25c365e38e476
L1ghtman2k <35672535+L1ghtman2k@users.noreply.github.com>
2021-05-01 20:17:07 -0400

    replace orange with black

--- web/src/components/ScoreBoard/Status.tsx
+++ web/src/components/ScoreBoard/Status.tsx
@@ -158,7 +158,7 @@ export default function Status(props: RanksProps) {
                                                 let style = {}
                                                 if (props.isDarkTheme){
                                                     if (data[name][column].Pause){
-                                                        style = {backgroundColor: "#DB4905"}
+                                                        style = {backgroundColor: "#000000"}
                                                     } else if (data[name][column].Check !== undefined && data[name][column].Check?.Passed){
                                                         style =  {backgroundColor: "#259B0B"}
                                                     } else{
@@ -166,7 +166,7 @@ export default function Status(props: RanksProps) {
                                                     }
                                                 } else{
                                                     if (data[name][column].Pause){
-                                                        style = {backgroundColor: "orange"}
+                                                        style = {backgroundColor: "#000000"}
                                                     } else if (data[name][column].Check !== undefined && data[name][column].Check?.Passed){
                                                         style =  {backgroundColor: "green"}
                                                     } else{
