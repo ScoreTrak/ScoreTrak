f54142f8330d5ce6351fa6eae1540145da629fde
L1ghtman2k <aibek.zhil@gmail.com>
2021-05-30 13:39:21 -0400

    Move web/ into its own repo under ScoreTrak/ScoreTrak-Client

--- web/src/styles/styles.tsx
+++
@@ -1,9 +0,0 @@
-import {makeStyles} from "@material-ui/core/styles";
-
-export const centerStyle = makeStyles((theme) => ({
-    alignItemsAndJustifyContent: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-}));
