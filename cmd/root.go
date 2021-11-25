bcf6b49ac9720b61b3bd1a4a8f5353bde6ad7493
Ibrahim Diallo <20114089+thisisibrahimd@users.noreply.github.com>
2021-11-25 12:56:44 -0500

    update --config description

--- cmd/root.go
+++ cmd/root.go
@@ -40,7 +40,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.scoretrak.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is './.scoretrak.yaml', '$HOME/.scoretrak.yaml', '/etc/scoretrak/.scoretrak.yaml' in that order)")
 	rootCmd.PersistentFlags().StringVar(&encodedCfg, "encoded-config", "", "base64 encoded config")
 
 	// Cobra also supports local flags, which will only run
